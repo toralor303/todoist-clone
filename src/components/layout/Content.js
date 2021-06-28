202ca4329ba58870116ae75f29d489109185c452
Charles <charlessorel97@gmail.com>
2021-06-27 21:31:41 -0400

    added firebase and node-sass

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Sidebar } from './Sidebar';
 
-export const Content = () => {
-    
-}
+export const Content = () => (
+    <section>
+        <Sidebar />
+    </section>
+)
